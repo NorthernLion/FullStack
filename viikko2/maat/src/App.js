c8881eb5ec06a3fbb76c4907b5d95fa7f2bd3180
NorthernLion <joppetaja@gmail.com>
2018-01-23 15:54:15 +0200

    2.16 lol it went trough

--- viikko2/maat/src/App.js
+++ viikko2/maat/src/App.js
@@ -6,7 +6,8 @@ class App extends React.Component {
     super()
     this.state = {
       countries: [],
-      filter: ''
+      filter: '',
+      country: '',
     }
   }
 
@@ -20,11 +21,51 @@ class App extends React.Component {
   }
 
   handleFilterChange = (event) => {
-    this.setState({ filter: event.target.value })
+    this.setState({ 
+      filter: event.target.value,
+      country: ''
+    })
     console.log(this.state.filter)
   }
 
+  handleCountryChange = (event) => {
+    this.setState({ 
+      country: event.target.value,
+      filter: ''
+    })
+    console.log(event.target.value)
+  }  
 
+  filter = (filter, countries) => {
+    return (countries.filter(country => country.name.toLowerCase().includes(filter.toLowerCase())))
+  }
+
+
+  countries = ({ countries }) => {
+    if (this.state.country.length > 2) {
+      const theOne = this.filter(this.state.country, this.state.countries)
+      console.log(theOne)
+      return (<FullCountry country={theOne[0]} />)
+    } else if (OverTen(countries) || countries === null) {
+      console.log('over ten')
+      return (<div>too many matches, specify another filter</div>)
+    } else if (One(countries)) {
+      console.log('onecountry only activated')
+      return (<FullCountry country={countries[0]} />)
+    } else {
+      return (
+        countries.map(country => <this.country country={country} />)
+      )
+    }
+  }
+
+  country = ({ country }) => {
+    return (
+      <div>
+        <button value={country.name} onClick={this.handleCountryChange}>{country.name}</button>
+      </div>
+    )
+  }
 
   render() {
     console.log('rendering')
@@ -35,22 +76,8 @@ class App extends React.Component {
           value={this.state.filter}
           onChange={this.handleFilterChange}
         />
-        <Countries countries={Filter(this.state.filter, this.state.countries)} />
+        <this.countries countries={this.filter(this.state.filter, this.state.countries)} />
       </div>
-    )
-  }
-}
-
-const Countries = ({ countries }) => {
-  if (OverTen(countries) || countries === null) {
-    console.log('over ten')
-    return (<div>too many matches, specify another filter</div>)
-  } else if (One(countries)) {
-    console.log('onecountry only activated')
-    return (<FullCountry country={countries[0]} />)
-  } else {
-    return (
-      countries.map(country => <Country country={country} />)
     )
   }
 }
@@ -66,19 +93,10 @@ const FullCountry = ({ country }) => {
   )
 }
 
-const Country = ({ country }) => {
-  return (
-    <div>{country.name}</div>
-  )
-}
-
 const OverTen = (countries) => countries.length > 10
 
 const One = (countries) => countries.length === 1
 
-const Filter = (filter, countries) => {
-  return (countries.filter(country => country.name.toLowerCase().includes(filter.toLowerCase())))
-}
 
 
 export default App
