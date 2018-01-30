5427c02dc3605e14e081a95faef2ccf5a5598a5a
NorthernLion <joppetaja@gmail.com>
2018-01-30 11:47:39 +0200

    4.2

--- viikko4/blogilista/index.js
+++ viikko4/blogilista/index.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -12,23 +12,7 @@ const mongoUrl = 'mongodb://admin:muumio@ds119028.mlab.com:19028/bloglistausdb_d
 mongoose.connect(mongoUrl)
 mongoose.Promise = global.Promise
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs.map(Blog.format))
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(Blog.format(result))
-    })
-})
+app.use('api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
