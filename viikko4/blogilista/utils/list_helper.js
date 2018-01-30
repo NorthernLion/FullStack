553f1f5cd62baa4ef55d83e03d831386abb12b12
NorthernLion <joppetaja@gmail.com>
2018-01-30 14:57:43 +0200

    4.5

--- viikko4/blogilista/utils/list_helper.js
+++ viikko4/blogilista/utils/list_helper.js
@@ -9,7 +9,15 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
 }
 
+
+const favoriteBlog = (blogs) => {
+  let likes = blogs.map(blog => blog.likes)
+  let maxLikes = Math.max(...likes)
+  let favorite = blogs.find(element => element.likes === maxLikes)
+  return favorite
+}
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
