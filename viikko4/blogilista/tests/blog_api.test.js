b8416214a639f47b76bb892a9af2b015e4eeaf09
NorthernLion <joppetaja@gmail.com>
2018-01-31 14:21:15 +0200

    4.12

--- viikko4/blogilista/tests/blog_api.test.js
+++ viikko4/blogilista/tests/blog_api.test.js
@@ -2,146 +2,110 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const { initialBlogs, blogsInDb } = require('./test_helper')
 
-const blogs = [
-  {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7
-  },
-  {
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5
-  },
-  {
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12
-  },
-  {
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10
-  },
-  {
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0
-  },
-  {
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2
-  }
-]
+describe('when there is initially some blogs saved', async () => {
+  beforeAll(async () => {
+    await Blog.remove({})
 
-
-beforeAll(async () => {
-  await Blog.remove({})
-
-  const blogObjects = blogs.map(blog => new Blog(blog))
-  const promiseArray = blogObjects.map(blog => blog.save())
-  await Promise.all(promiseArray)
-})
-
-describe('when GET request is made, ', () => {
-
-  test('blogs are returned as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+    const blogObjects = initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
   })
 
-  test('the amount of returned blogs is equal to the amount in database', async () => {
-    const response = await api
-      .get('/api/blogs')
-    expect(response.body.length).toBe(blogs.length)
+  describe('when GET request is made to api/blogs, ', () => {
+
+    test('blogs are returned as json', async () => {
+      await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
+
+    test('the amount of returned blogs is equal to the amount in database', async () => {
+      const blogInDatabase = await blogsInDb()
+      
+      const response = await api
+        .get('/api/blogs')
+      expect(response.body.length).toBe(blogInDatabase.length)
+    })
+
+    test("the author of first blog is Michael Chan", async () => {
+      const response = await api
+        .get('/api/blogs')
+
+      expect(response.body[0].author).toBe('Michael Chan')
+    })
   })
 
-  test("the author of first blog is Michael Chan", async () => {
-    const response = await api
-      .get('/api/blogs')
+  describe('when POST request is made to api/blogs ', () => {
+    test('a valid blog can be added', async () => {
+      const blogsAtStart = await blogsInDb()
 
-    expect(response.body[0].author).toBe('Michael Chan')
+      const newBlog = {
+        "title": "how to eat your face",
+        "author": "zoiberg",
+        "url": "google.fi",
+        "likes": 20
+      }
+      const postResponse = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const blogsAfterOperation = await blogsInDb()
+
+      const titles = blogsAfterOperation.map(r => r.title)
+
+      expect(blogsAfterOperation.length).toBe(blogsAtStart.length + 1)
+      expect(blogsAfterOperation.map(Blog.formatNoId)).toContainEqual(newBlog)
+      expect(titles).toContainEqual(newBlog.title)
+    })
+
+    test("a blog without title is not added", async () => {
+      const blogsAtStart = await blogsInDb()
+      newBlog = {
+        "author": "zoiberg",
+        "url": "google.fi",
+        "likes": 20
+      }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+
+      const blogsAfterOperation = await blogsInDb()
+
+      expect(blogsAfterOperation.length).toBe(blogsAtStart.length)
+      expect(blogsAfterOperation.length).toEqual(blogsAtStart.length)
+    })
+
+    test("a blog without likes is added and gets value of 0 likes", async () => {
+      const blogsAtStart = await blogsInDb()
+
+      newBlog = {
+        "title": "how to eat your face",
+        "author": "zoiberg",
+        "url": "google.fi",
+      }
+      const postResponse = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+
+      const blogsAfterOperation = await blogsInDb()
+
+      expect(blogsAfterOperation.length).toBe(blogsAtStart.length + 1)
+
+      expect(postResponse.body.likes).toEqual(0)
+    })
   })
-})
 
-describe('when POST request is made, ', () => {
-  test('a valid blog can be added', async () => {
-    const newBlog = {
-      "title": "how to eat your face",
-      "author": "zoiberg",
-      "url": "google.fi",
-      "likes": 20
-    }
-    const postResponse = await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-
-    const response = await api
-      .get('/api/blogs')
-
-    const contents = response.body.map(r => r.content)
-
-    expect(response.body.length).toBe(blogs.length + 1)
-    expect(Blog.formatNoId(response.body[response.body.length - 1])).toEqual(Blog.formatNoId(newBlog))
-    expect(Blog.formatNoId(postResponse.body)).toEqual(Blog.formatNoId(newBlog))
+  afterAll(() => {
+    server.close()
   })
-
-  test("a blog without title is not added", async () => {
-    newBlog = {
-      "author": "zoiberg",
-      "url": "google.fi",
-      "likes": 20
-    }
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-
-
-    const response = await api
-      .get('/api/blogs')
-
-    const contents = response.body.map(r => r.content)
-
-    expect(response.body.length).toBe(blogs.length + 1)
-  })
-
-  test("a blog without likes is added and gets value of 0 likes", async () => {
-    newBlog = {
-      "title": "how to eat your face",
-      "author": "zoiberg",
-      "url": "google.fi",
-    }
-    const postResponse = await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-
-    const response = await api
-      .get('/api/blogs')
-
-    const contents = response.body.map(r => r.content)
-
-    expect(response.body.length).toBe(blogs.length + 2)
-    expect(Blog.formatNoId(postResponse.body).likes).toEqual(0)
-  })
-})
-
-afterAll(() => {
-  server.close()
 })
