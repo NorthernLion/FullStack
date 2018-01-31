4d793418039e0caa840d69398847ed21273bfc97
NorthernLion <joppetaja@gmail.com>
2018-01-31 19:35:38 +0200

    4.19

--- viikko4/blogilista/controllers/blogs.js
+++ viikko4/blogilista/controllers/blogs.js
@@ -1,11 +1,20 @@
 const Blog = require('../models/blog')
 const blogsRouter = require('express').Router()
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
     .find({})
-    .populate("user", {username: 1, name: 1})
+    .populate("user", { username: 1, name: 1 })
   response.json(blogs.map(Blog.format))
 })
 
@@ -22,13 +31,13 @@ blogsRouter.delete('/:id', async (request, response) => {
 blogsRouter.put('/:id', async (request, response) => {
   try {
     const body = request.body
-    const blog =  new Blog ({
+    const blog = new Blog({
       _id: request.params.id,
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes
-    })    
+    })
     const savedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
     response.status(201).json(Blog.format(savedBlog))
   } catch (exception) {
@@ -38,13 +47,19 @@ blogsRouter.put('/:id', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+
   try {
-    const body = request.body
-    if (body.title === undefined || body.url === undefined) {
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    if (body.title === undefined ||  body.url === undefined) {
       return response.status(400).json({ error: `You need to give title and url in order to post a blog` })
     }
 
-    const user = await User.findById(body.userId)
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       title: body.title,
@@ -58,12 +73,16 @@ blogsRouter.post('/', async (request, response) => {
 
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
-    
+
     response.status(201).json(Blog.format(savedBlog))
 
   } catch (exception) {
-    console.log(exception)
-    response.status(500).json({ error: `something went wrong...` })
+    if (exception.name === 'JsonWebTokenError') {
+      response.status(401).json({ error: exception.message })
+    } else {
+      console.log(exception)
+      response.status(500).json({ error: `something went wrong...` })
+    }
   }
 })
 
