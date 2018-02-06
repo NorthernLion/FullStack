a7f2bfea0b2387634579fd0e4e14b8a58afc9b55
NorthernLion <joppetaja@gmail.com>
2018-02-06 19:20:33 +0200

    material tasks

---
+++ viikko5/part2-notes-part5-1/src/components/Note.test.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Note from './Note'
+
+describe.only('<Note />', () => {
+  const note = {
+    content: 'Komponenttitestaus tapahtuu jestillä ja enzymellä',
+    important: true
+  }
+  it('renders content', () => {
+
+    const noteComponent = shallow(<Note note={note} />)
+    const contentDiv = noteComponent.find('.content')
+    /*
+    console.log(contentDiv.debug())
+    console.log(noteComponent.debug())
+    */
+    expect(contentDiv.text()).toContain(note.content)
+  })
+  it('clicking the button calls event handler once', () => {
+    const mockHandler = jest.fn()
+
+    const noteComponent = shallow(
+      <Note
+        note={note}
+        toggleImportance={mockHandler}
+      />
+    )
+
+    const button = noteComponent.find('button')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(1)
+  })
+})
