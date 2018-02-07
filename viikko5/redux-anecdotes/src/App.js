bad50e72c4f29999c1941ce8e45d7310a4fc76df
NorthernLion <joppetaja@gmail.com>
2018-02-07 15:39:34 +0200

    5.19

---
+++ viikko5/redux-anecdotes/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import actionFor from './actionCreators'
+
+class App extends React.Component {
+  addVote = (id) => () => {
+    this.props.store.dispatch(actionFor.addVote(id))
+  }
+
+  render() {
+    const anecdotes = this.props.store.getState()
+    return (
+      <div>
+        <h2>Anecdotes</h2>
+        {anecdotes.map(anecdote=>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content} 
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={this.addVote(anecdote.id)}>vote</button>
+            </div>
+          </div>
+        )}
+        <h2>create new</h2>
+        <form>
+          <div><input /></div>
+          <button>create</button> 
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ viikko5/redux-anecdotes/src/actionCreators.js
@@ -0,0 +1,9 @@
+
+export default {
+  addVote(id) {
+    return {
+      type: 'ADD_VOTE',
+      data: { id }
+    }
+  }
+}
